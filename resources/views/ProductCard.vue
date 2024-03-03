d322759cc8691efb2e6f58887ecd7065c3c05366
thabzzzzz <tmosenohi99@gmail.com>
2024-03-03 03:23:23 +0200

    need to style cards

--- resources/views/ProductCard.vue
+++ resources/views/ProductCard.vue
@@ -1,21 +1,27 @@
 <template>
-  <div v-if="games && games.results && games.results.length">
-    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-10">
-      <div v-for="game in games.results" :key="game.id" class="card border border-black mx-auto product-card" style="width: 21rem;">
-        <img :src="getCoverImageUrl(game)" class="card-img-top" :alt="game.name">
-        <div class="card-body product-card-body p-2">
-          <b class="card-title">{{ game.name }}</b>
-          <br>
-          <span v-for="(platform, index) in game.platforms" :key="index">{{ platform.platform.name }}</span>
-          <br>
-          <br>
-          <div class="description-text clamp-description"><i class="card-text">{{ game.summary }}</i></div>
-          <br>
-          <span class="card-price">Price: N/A</span>
-          <br>
-          <a :href="game.url" class="btn my-btn-2 text-black inline-block mr-1" target="_blank" title="Visit site">
-            Visit Game
-          </a>
+  <div>
+    <div v-if="!games || games.length === 0">
+      <!-- Show loading message -->
+      <p>Loading...</p>
+    </div>
+    <div v-else>
+      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-10">
+        <div v-for="game in games" :key="game.id" class="card border border-black mx-auto product-card" style="width: 21rem;">
+          <img :src="getCoverImageUrl(game)" class="card-img-top" :alt="game.name">
+          <div class="card-body product-card-body p-2">
+            <b class="card-title">{{ game.name }}</b>
+            <br>
+            <span v-for="(platform, index) in game.platforms" :key="index">{{ platform.platform.name }}</span>
+            <br>
+            <br>
+            <div class="description-text clamp-description"><i class="card-text">{{ game.summary }}</i></div>
+            <br>
+            <span class="card-price">Price: N/A</span>
+            <br>
+            <a :href="game.url" class="btn my-btn-2 text-black inline-block mr-1" target="_blank" title="Visit site">
+              Visit Game
+            </a>
+          </div>
         </div>
       </div>
     </div>
@@ -23,28 +29,33 @@
 </template>
 
 
+
 <script>
 export default {
   props: {
     games: {
-      type: Array,
-      required: true
-    }
+    type: Array,
+    required: true,
+  },
   },
   methods: {
     getCoverImageUrl(game) {
       return game.background_image || 'placeholder_image_url.jpg';
-    }
+    },
   },
   mounted() {
-    console.log('Games prop:', this.games);
+    console.log('games prop:', this.games); // Check the initial state of the prop
+
+    // Implement data fetching logic here (if not done in a parent component)
+    // Use tools like browser developer tools to inspect network requests and responses
+
     if (this.games && this.games.results) {
       console.log('Results:', this.games.results);
     } else {
       console.log('Games or Results is undefined or empty:', this.games);
     }
-  }
-}
+  },
+};
 </script>
 
 <style scoped>
